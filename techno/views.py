5ed6f4b15c7bff1d5998ddd704412cb8fba62bbe
Patarimi <mpqqch@gmail.com>
2020-07-27 10:13:18 +0200

    first dev

--- techno/views.py
+++ techno/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from techno.models import Components_Type, Component
 
 def index(request):
-    return render(request, "index.html")
-
+    context = {
+        "compo_num": Component.objects.all().count(),
+        "compo_type_num": Components_Type.objects.all().count(),
+        }
+    return render(request, "index.html", context)
 
 def explore(request):
+    
     return render(request, "explore.html")
+
+class ListCompoType(generic.ListView):
+    model = Components_Type
+    template_name = "list.html"
+
+class ListCompo(generic.ListView):
+    model = Component
+    template_name = "list.html"
