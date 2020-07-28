7a97984e929b9be961668467584f9c3449eda0ce
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-07-28 09:30:40 +0200

    Update admin.py

--- techno/admin.py
+++ techno/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Techno, Provider, Components_Type, Component
+from .models import Techno, Provider, Components_Type, Component, Ker_perf
 
 admin.site.register(Techno)
 admin.site.register(Provider)
 admin.site.register(Components_Type)
 admin.site.register(Component)
+admin.site.register(Key_Perf)
