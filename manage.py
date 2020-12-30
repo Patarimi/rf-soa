ec5cbb6a1e5925d77eefe6bee5f98fc163757280
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-12-30 14:54:38 +0100

    rename app name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rf_soa.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rf_insight.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
