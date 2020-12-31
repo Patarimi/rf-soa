3d4cbc0a8a8933b4993188bfa2f633b5ce58f025
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-12-31 08:15:06 +0100

    first test for rd_design
    + full coverage report

--- rf_design/tests.py
+++ rf_design/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .views import index
+
+class TestViews(TestCase):
+    def test_index(self):
+        resp = self.client.get(reverse('rf:index'))
+        self.assertEqual(resp.status_code, 200) 
