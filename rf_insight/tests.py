d62ce5ec477bb43190a0e3e3fffa0823744b5fbe
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-12-31 08:34:41 +0100

    cmd fix

---
+++ rf_insight/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .views import index
+
+class TestViews(TestCase):
+    def test_index(self):
+        resp = self.client.get(reverse('rf:index'))
+        self.assertEqual(resp.status_code, 200)
