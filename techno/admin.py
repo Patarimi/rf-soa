d291fd70435991e3be2102383b638fb42fac3d31
Patarimi <38954040+Patarimi@users.noreply.github.com>
2020-12-31 15:54:40 +0100

    Update admin.py

--- techno/admin.py
+++ techno/admin.py
@@ -4,30 +4,27 @@ from .models import Technos_Type, Techno, Provider, Components_Type, Component, 
 
 class Key_PerfInline(admin.TabularInline):
     model = Key_Perf
-    extra = 3
-    
+    extra = 1
+
+@admin.register(Component)    
 class ComponentAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Description', {'fields':['doi', 'comp_type_id', 'techno']}),
-        #('Performances', {'fields':['perf_record'], 'classes':['collapse']}),
-    ]
+    inlines = (Key_PerfInline, )
     list_display = ('doi', 'comp_type_id', 'techno')
     list_filter = ['comp_type_id', 'techno']
 
+@admin.register(Key_Param)
 class Key_ParamAdmin(admin.ModelAdmin):
     list_display = ('name_text', 'units')
 
+@admin.register(Key_Perf)
 class Key_PerfAdmin(admin.ModelAdmin):
     list_display = ('component', 'key_param', 'value')
     list_fiter = ('component', 'key_param')
 
+@admin.register(Techno)
 class TechnoAdmin(admin.ModelAdmin):
     list_display = ('name', 'provider', 'node_length', 'techno_type')
 
 admin.site.register(Technos_Type)
-admin.site.register(Techno, TechnoAdmin)
 admin.site.register(Provider)
 admin.site.register(Components_Type)
-admin.site.register(Component, ComponentAdmin)
-admin.site.register(Key_Perf, Key_PerfAdmin)
-admin.site.register(Key_Param, Key_ParamAdmin)
